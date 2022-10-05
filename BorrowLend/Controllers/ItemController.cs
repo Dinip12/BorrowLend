9c79473d9830dda8d65e77779275db5ebc166007
pgee <pgee>
2022-10-05 12:14:59 +0300

    Update/Delete

--- BorrowLend/Controllers/ItemController.cs
+++ BorrowLend/Controllers/ItemController.cs
@@ -1,16 +1,71 @@
 ﻿using BorrowLend.Data;
 using BorrowLend.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity.Migrations;
 
 namespace BorrowLend.Controllers
 {
     public class ItemController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         public IActionResult Index()
         {
-            ApplicationDbContext applicationDbContext = new ApplicationDbContext();
-            return View(applicationDbContext.Items);
+            
+            return View(db.Items);
         }
-
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item item)
+        {
+            db.Items.Add(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Update(int? id)
+        {
+            var obj = db.Items.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Item obj)
+        {           
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            db.Items.AddOrUpdate(obj);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int? id)
+        {
+            var obj = db.Items.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Item obj)
+        {
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            db.Items.Remove(obj);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
